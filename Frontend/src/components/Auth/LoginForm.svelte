3daf1528a0b0b99ac66f3416b8fb57f1279cae13
Achraf EL ALLALI <aelallali@ctcpa.org>
2026-03-10 14:08:13 +0100

    setting up basin login input form

---
+++ Frontend/src/components/Auth/LoginForm.svelte
@@ -0,0 +1,5 @@
+<div class="LoginBox">
+    <input type="text" placeholder="Username" class="Input" />
+    <input type="password" placeholder="Password" class="Input" />
+    <button class="LoginButton">Login</button>
+</div>
---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/login">Login</a>
+</nav>
+
+{@render children()}
