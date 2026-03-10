444cd3b838549a56339c6855b8695ca972955877
Achraf EL ALLALI <aelallali@ctcpa.org>
2026-03-10 16:07:39 +0100

    creating login form component

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script>
+	import "../app.css";
 	let { children } = $props();
 </script>
-
-<nav>
-	<a href="/">Home</a>
-	<a href="/login">Login</a>
-</nav>
 
 {@render children()}
--- Frontend/src/routes/login/+page.svelte
+++ Frontend/src/routes/login/+page.svelte
@@ -2,7 +2,6 @@
   import LoginForm from '../../components/Auth/LoginForm.svelte';
 </script>
 
-<h1>Welcome to Auth page</h1>
 <div class="AuthPage">
     <LoginForm />
 </div>
