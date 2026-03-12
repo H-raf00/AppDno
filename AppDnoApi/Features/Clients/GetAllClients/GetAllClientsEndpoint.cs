cbc2ddb92350806be563f8ce80fcee66ef25ccb7
Achraf EL ALLALI <aelallali@ctcpa.org>
2026-03-12 16:35:43 +0100

    adding an endpoint to get all clients

---
+++ AppDnoApi/Features/Clients/GetAllClients/GetAllClientsEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using AppDnoApi.Database;
+using AppDnoApi.Features.Clients.GetAllClient;
+using AppDnoApi.Features.Users.GetAllUsers;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppDnoApi.Features.Clients.GetClient
+{
+    public class GetAllClientsEndpoint : EndpointWithoutRequest<List<GetAllClientsResponse>>
+    {
+        private readonly AppDnoDbContext _dbContext;
+
+        public GetAllClientsEndpoint(AppDnoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public override void Configure()
+        {
+            Get("/api/client/all");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            List<GetAllClientsResponse> users = await _dbContext.Clients
+                .Select(c => new GetAllClientsResponse
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                })
+                .ToListAsync(ct);
+
+            await Send.OkAsync(users);
+        }
+    }
+}
+
---
+++ AppDnoApi/Features/Clients/GetAllClients/GetAllClientsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AppDnoApi.Features.Clients.GetAllClient
+{
+    public class GetAllClientsResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
